e86a640e5f049e95452d2306a0ae6ebc3e4c5b00
Luigi <lbhsg520741778@gmail.com>
2025-11-18 14:30:50 -0300

    Adicionando home message

---
+++ src/main/java/com/softcode/nextstep/api/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.softcode.nextstep.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    
+    @GetMapping("/")
+    public String msgBoasVindas(){
+        return "Bem vindo à nossa API";
+    }
+
+}
