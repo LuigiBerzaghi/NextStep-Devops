6a7a58a9494fa6c455a3fe60d899f1e4a4aa7348
Luigi <lbhsg520741778@gmail.com>
2025-11-19 11:10:27 -0300

    Removendo RabbitMQ local2

--- src/main/java/com/softcode/nextstep/messaging/NotificationListener.java
+++ src/main/java/com/softcode/nextstep/messaging/NotificationListener.java
@@ -2,9 +2,11 @@ package com.softcode.nextstep.messaging;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConditionalOnProperty(name = "messaging.notifications.enabled", havingValue = "true", matchIfMissing = true)
 @Slf4j
 public class NotificationListener {
 
